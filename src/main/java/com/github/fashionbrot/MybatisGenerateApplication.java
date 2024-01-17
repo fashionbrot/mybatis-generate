c37b20b43f26db5828ac8f85c9d0fa40669562b7
fashionbrot <fashionbrot@163.com>
2024-01-17 11:30:30 +0800

    增加 打包exe文件配置，以及默认打开浏览器

--- src/main/java/com/github/fashionbrot/MybatisGenerateApplication.java
+++ src/main/java/com/github/fashionbrot/MybatisGenerateApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.util.Date;
 
@@ -27,6 +28,23 @@ public class MybatisGenerateApplication {
         String port = environment.getProperty("server.port");
         log.info("启动完成:{}  访问端口:{}", DateFormatUtils.format(new Date(),"yyyy-MM-dd HH:mm:ss"),environment.getProperty("server.port"));
         System.out.println("http://localhost:"+port+"");
+    }
+
+
+    @PostConstruct
+    public void init() {
+        try {
+            String port = environment.getProperty("server.port");
+            String url = "http://localhost:" + port;
+            String os = System.getProperty("os.name").toLowerCase();
+            if (os.contains("win")) {
+                Runtime rt = Runtime.getRuntime();
+                // For Windows
+                rt.exec("rundll32 url.dll,FileProtocolHandler " + url);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
